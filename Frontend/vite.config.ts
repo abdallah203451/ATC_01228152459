103732a300872d80945bf5e6552a9e7f87b36149
unknown <abdallahashraf203@gmail.com>
2025-05-16 04:31:58 +0300

    Configure production env in Vite

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "ticket-central-tagger";
+// import { componentTagger } from "ticket-central-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -16,9 +16,9 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  // plugins: [react(), mode === "development" && componentTagger()].filter(
+  //   Boolean
+  // ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
